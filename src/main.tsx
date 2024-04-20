5e39f74099deafa9935ef648e988604ddd0cbc6e
Oleg Somov <gog0125@mail.ru>
2024-04-20 15:01:04 +0300

    first app setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'normalize.css';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
