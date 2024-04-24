540b3ed635258a85a0ec483a1ae0233c1500863c
Oleg Somov <gog0125@mail.ru>
2024-04-24 20:12:08 +0300

    add base path for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  base: '/singularis-test-2024/',
   resolve: {
     alias: {}
   }
