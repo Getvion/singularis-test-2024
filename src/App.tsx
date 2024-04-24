3b3fb87de98f5c5cc1bb5cc20e4405a52635fa6a
Oleg Somov <gog0125@mail.ru>
2024-04-24 20:03:02 +0300

    add main styles and main strucrure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
-const App = () => <div>content</div>;
+import { useState } from 'react';
 
-export default App;
+import { Title } from './shared';
+import { Form } from './widgets';
+
+export const App = () => {
+  const [timeTargetDate, setTimeTargetDate] = useState<number>(0);
+
+  return (
+    <div className='app'>
+      <div className='container'>
+        <Title>Тестовый Банк</Title>
+        <Form timeTargetDate={timeTargetDate} setTimeTargetDate={setTimeTargetDate} />
+      </div>
+    </div>
+  );
+};
