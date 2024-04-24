1583d6ba942e6700bdeb41974ed6a5414a64fd59
Oleg Somov <gog0125@mail.ru>
2024-04-24 20:00:24 +0300

    add ref props

--- src/shared/UI/Input/Input.tsx
+++ src/shared/UI/Input/Input.tsx
@@ -1,23 +1,28 @@
 import clsx from 'clsx';
+import { forwardRef } from 'react';
 
 import { InputTypeFields } from '../../../@types/Enums';
 
 import styles from './input.module.scss';
 
-interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+interface InputProps extends React.ComponentPropsWithRef<'input'> {
   type: InputTypeFields;
 }
 
-export const Input = ({ type, name, onChange, value, placeholder }: InputProps) => (
-  <input
-    className={clsx(styles.input, {
-      [styles.number]: type === 'number',
-      [styles.email]: type === 'email'
-    })}
-    type={type}
-    name={name}
-    placeholder={placeholder}
-    value={value}
-    onChange={onChange}
-  />
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type, name, onChange, value, placeholder, required }, ref) => (
+    <input
+      className={clsx(styles.input, {
+        [styles.number]: type === 'number',
+        [styles.email]: type === 'email'
+      })}
+      ref={ref}
+      required={required}
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  )
 );
